10ed5f59c2199ac940deedd9e6e7436e6dfa36cf
jihoon <jihoon.kwon@getlinq.com>
2024-10-13 23:54:24 +0900

    new code

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,55 +1,21 @@
-from src.datasets.base import (
-    TextDatasetFeaturesWithImage,
-    TextDatasetFeaturesWithImageURL,
-    SequenceTextDatasetFeaturesWithImage,
-    SequenceTextDatasetFeaturesWithImageURL,
-    HardSequenceTextDatasetFeaturesWithImage,
-    HardSequenceTextDatasetFeaturesWithImageURL,
-    NegCLIPTextDatasetFeaturesWithImageURL,
-    NegCLIPSequenceTextDatasetFeaturesWithImageURL,
-    BaseBuilder,
-)
-from src.datasets.builder import (
-    TextDatasetFeaturesWithImageBuilder,
-    TextDatasetFeaturesWithImageURLBuilder,
-    SequenceTextDatasetFeaturesWithImageBuilder,
-    SequenceTextDatasetFeaturesWithImageURLBuilder,
-    HardSequenceTextDatasetWithImageBuilder,
-    HardSequenceTextDatasetFeaturesWithImageURLBuilder,
-    NegCLIPTextDatasetFeaturesWithImageURLBuilder,
-    NegCLIPSequenceTextDatasetFeaturesWithImageURLBuilder,
-)
+from src.datasets.base import BaseBuilder
 from src.datasets.aro import ARODatasetBuilder
 from src.datasets.coco_captions import (
     COCOCaptionsDatasetBuilder,
-    COCOCaptionsIterableDatasetBuilder,
     COCOCaptionsDatasetBuilderWithNegCLIPMining,
     COCOCaptionsDatasetBuilderWithMinedNegCLIP
 )
 from src.datasets.conceptual_captions import (
-    ConceptualCaptionsIterableDatasetBuilder,
     ConceptualCaptionsDatasetBuilder
 )
 from src.datasets.crepe import CREPEDatasetBuilder
-from src.datasets.sbu_captions import SBUCaptionsDatasetBuilder
 from src.datasets.flickr30k import Flickr30kDatasetBuilder
 from src.datasets.sugarcrepe import SUGARCREPEDatasetBuilder
 from src.datasets.svo import SVODatasetBuilder
 from src.datasets.winoground import WinogroundDatasetBuilder
-from src.datasets.laion import (
-    Laion400mIterableDatasetBuilder, Laion400mDatasetBuilder, Laion400mTarPathDatasetBuilder
-)
 
 __all__ = [
     'BaseBuilder',
-    'TextDatasetFeaturesWithImageBuilder',
-    'TextDatasetFeaturesWithImageURLBuilder',
-    'SequenceTextDatasetFeaturesWithImageBuilder',
-    'SequenceTextDatasetFeaturesWithImageURLBuilder',
-    'HardSequenceTextDatasetWithImageBuilder',
-    'HardSequenceTextDatasetFeaturesWithImageURLBuilder',
-    'NegCLIPTextDatasetFeaturesWithImageURLBuilder',
-    'NegCLIPSequenceTextDatasetFeaturesWithImageURLBuilder',
     'ARODatasetBuilder',
     'COCOCaptionsDatasetBuilder',
     'COCOCaptionsDatasetBuilderWithNegCLIPMining',
