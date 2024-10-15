0ce73f4ec50e9d2f624205a264a1bb8dc4acafdc
jihoon <jihoon.kwon@getlinq.com>
2024-10-15 17:01:15 +0900

    new code

--- src/runners/trainer.py
+++ src/runners/trainer.py
@@ -1,11 +1,16 @@
+from typing import Optional, Dict
+
 import torch
 from torch.utils.data import Dataset, RandomSampler
-from typing import Optional
+from transformers import is_torch_xla_available
 from transformers.trainer_utils import has_length
 
 from src.common.registry import registry
 from src.models.modeling_base import contrastive_loss
 from src.runners.base import BaseTrainer
+
+if is_torch_xla_available():
+    import torch_xla.core.xla_model as xm
 
 
 def neg_clip_loss(similarity: torch.Tensor) -> torch.Tensor:
@@ -69,6 +74,90 @@ class RandomSamplerTrainer(BaseTrainer):
         loss = outputs.loss
 
         return (loss, outputs) if return_outputs else loss
+
+
+@registry.register_trainer('RandomSamplerWithMultiLossTrainer')
+class RandomSamplerWithMultiLossTrainer(RandomSamplerTrainer):
+    """
+    A subclass of the `RandomSamplerTrainer` that overrides the method for
+    getting the multi-loss used for the training dataset.
+    """
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        """
+        Computes the loss for the current batch of inputs using the negative CLIP loss function.
+
+        Args:
+            model: The model to be used for generating the logits.
+            inputs: A dictionary of inputs that includes features such as images and captions.
+            return_outputs (`bool`, *optional*, defaults to `False`): If `True`, returns both the loss and the model outputs.
+
+        Returns:
+            `torch.Tensor` or `Tuple[torch.Tensor, Any]`: If `return_outputs` is `True`, returns a tuple of (loss, outputs).
+            Otherwise, returns only the loss.
+        """
+        inputs = dict(inputs, **{
+            'return_dict': True,
+            'return_loss': True,
+        })
+        outputs = model(**inputs)
+        loss = outputs.loss
+        lm_loss = outputs.lm_loss
+        cont_loss = outputs.contrastive_loss
+
+        self.state.lm_loss = getattr(
+            self.state, "lm_loss", torch.tensor(0.0).to(lm_loss.device)
+        )
+        self.state.cont_loss = getattr(
+            self.state, "contrastive_loss", torch.tensor(0.0).to(cont_loss.device)
+        )
+        self.state.lm_loss += lm_loss.detach()
+        self.state.cont_loss += cont_loss.detach()
+
+        return (loss, outputs) if return_outputs else loss
+
+    def _maybe_log_save_evaluate(self, tr_loss, grad_norm, model, trial, epoch, ignore_keys_for_eval):
+        if self.control.should_log and self.state.global_step > self._globalstep_last_logged:
+            if is_torch_xla_available():
+                xm.mark_step()
+
+            logs: Dict[str, float] = {}
+
+            # all_gather + mean() to get average loss over all processes
+            tr_loss_scalar = self._nested_gather(tr_loss).mean().item()
+
+            # reset tr_loss to zero
+            tr_loss -= tr_loss
+
+            logs["loss"] = round(tr_loss_scalar / (self.state.global_step - self._globalstep_last_logged), 4)
+
+            if hasattr(self.state, "lm_loss") and hasattr(self.state, "contrastive_loss"):
+                tr_lm_loss_scalar = self._nested_gather(self.state.lm_loss).mean().item()
+                tr_contrastive_loss_scalar = self._nested_gather(self.state.contrastive_loss).mean().item()
+                logs["lm_loss"] = round(tr_lm_loss_scalar / (self.state.global_step - self._globalstep_last_logged), 4)
+                logs["contrastive_loss"] = round(tr_contrastive_loss_scalar / (self.state.global_step - self._globalstep_last_logged), 4)
+
+                # reset lm_loss and contrastive_loss to zero
+                self.state.lm_loss -= self.state.lm_loss
+                self.state.contrastive_loss -= self.state.contrastive_loss
+
+            if grad_norm is not None:
+                logs["grad_norm"] = grad_norm.detach().item() if isinstance(grad_norm, torch.Tensor) else grad_norm
+            logs["learning_rate"] = self._get_learning_rate()
+
+            self._total_loss_scalar += tr_loss_scalar
+            self._globalstep_last_logged = self.state.global_step
+            self.store_flos()
+
+            self.log(logs)
+
+        metrics = None
+        if self.control.should_evaluate:
+            metrics = self._evaluate(trial, ignore_keys_for_eval)
+
+        if self.control.should_save:
+            self._save_checkpoint(model, trial, metrics=metrics)
+            self.control = self.callback_handler.on_save(self.args, self.state, self.control)
 
 
 @registry.register_trainer('NegCLIPRandomSamplerTrainer')
