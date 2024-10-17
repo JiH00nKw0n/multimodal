d0b96a6e29878fe93dd2a4ac8c8c9fbbc9e77094
jihoon <jihoon.kwon@getlinq.com>
2024-10-17 14:47:59 +0900

    new code

--- src/models/clipt5/modeling_clipt5.py
+++ src/models/clipt5/modeling_clipt5.py
@@ -428,61 +428,65 @@ class CLIPT5ForConditionalGeneration(CLIPT5PreTrainedModel):
         image_embeds = image_embeds / image_embeds.norm(p=2, dim=-1, keepdim=True)
         text_embeds = text_embeds / text_embeds.norm(p=2, dim=-1, keepdim=True)
 
-        encoder_hidden_states = text_embeds.unsqueeze(1)
-        batch_size, _ = attention_mask.shape
-        encoder_attention_mask = torch.ones(batch_size, 1, dtype=attention_mask.dtype, device=attention_mask.device)
+        decoder_outputs = None
+        lm_logits = None
 
-        if self.text_model.model_parallel:
-            torch.cuda.set_device(self.text_model.decoder.first_device)
+        if self.training:
+            encoder_hidden_states = text_embeds.unsqueeze(1)
+            batch_size, _ = attention_mask.shape
+            encoder_attention_mask = torch.ones(batch_size, 1, dtype=attention_mask.dtype, device=attention_mask.device)
 
-        if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
-            # get decoder inputs from shifting lm labels to the right
-            decoder_input_ids = self.text_model._shift_right(labels)
+            if self.text_model.model_parallel:
+                torch.cuda.set_device(self.text_model.decoder.first_device)
 
-        # Set device for model parallelism
-        if self.text_model.model_parallel:
-            torch.cuda.set_device(self.decoder.first_device)
-            text_embeds = text_embeds.to(self.text_model.decoder.first_device)
-            if decoder_input_ids is not None:
-                decoder_input_ids = decoder_input_ids.to(self.text_model.decoder.first_device)
-            if attention_mask is not None:
-                attention_mask = attention_mask.to(self.text_model.decoder.first_device)
-            if decoder_attention_mask is not None:
-                decoder_attention_mask = decoder_attention_mask.to(self.text_model.decoder.first_device)
+            if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
+                # get decoder inputs from shifting lm labels to the right
+                decoder_input_ids = self.text_model._shift_right(labels)
 
-        # Decode
-        decoder_outputs = self.text_model.decoder(
-            input_ids=decoder_input_ids,
-            attention_mask=decoder_attention_mask,
-            inputs_embeds=decoder_inputs_embeds,
-            past_key_values=past_key_values,
-            encoder_hidden_states=encoder_hidden_states,
-            encoder_attention_mask=encoder_attention_mask,
-            head_mask=decoder_head_mask,
-            cross_attn_head_mask=cross_attn_head_mask,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
+            # Set device for model parallelism
+            if self.text_model.model_parallel:
+                torch.cuda.set_device(self.decoder.first_device)
+                text_embeds = text_embeds.to(self.text_model.decoder.first_device)
+                if decoder_input_ids is not None:
+                    decoder_input_ids = decoder_input_ids.to(self.text_model.decoder.first_device)
+                if attention_mask is not None:
+                    attention_mask = attention_mask.to(self.text_model.decoder.first_device)
+                if decoder_attention_mask is not None:
+                    decoder_attention_mask = decoder_attention_mask.to(self.text_model.decoder.first_device)
 
-        sequence_output = decoder_outputs[0]
+            # Decode
+            decoder_outputs = self.text_model.decoder(
+                input_ids=decoder_input_ids,
+                attention_mask=decoder_attention_mask,
+                inputs_embeds=decoder_inputs_embeds,
+                past_key_values=past_key_values,
+                encoder_hidden_states=encoder_hidden_states,
+                encoder_attention_mask=encoder_attention_mask,
+                head_mask=decoder_head_mask,
+                cross_attn_head_mask=cross_attn_head_mask,
+                use_cache=use_cache,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
 
-        # Set device for model parallelism
-        if self.text_model.model_parallel:
-            torch.cuda.set_device(self.text_model.encoder.first_device)
-            self.text_model.lm_head = self.lm_head.to(self.text_model.encoder.first_device)
-            sequence_output = sequence_output.to(self.text_model.lm_head.weight.device)
+            sequence_output = decoder_outputs[0]
 
-        if self.text_config.tie_word_embeddings:
-            # Rescale output before projecting on vocab
-            # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
-            sequence_output = sequence_output * (self.text_model.model_dim ** -0.5)
+            # Set device for model parallelism
+            if self.text_model.model_parallel:
+                torch.cuda.set_device(self.text_model.encoder.first_device)
+                self.text_model.lm_head = self.lm_head.to(self.text_model.encoder.first_device)
+                sequence_output = sequence_output.to(self.text_model.lm_head.weight.device)
 
-        lm_logits = self.text_model.lm_head(sequence_output)
+            if self.text_config.tie_word_embeddings:
+                # Rescale output before projecting on vocab
+                # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
+                sequence_output = sequence_output * (self.text_model.model_dim ** -0.5)
+
+            lm_logits = self.text_model.lm_head(sequence_output)
 
         lm_loss = None
-        if labels is not None:
+        if labels is not None and self.training:
             loss_fct = CrossEntropyLoss(ignore_index=-100)
             # move labels to correct device to enable PP
             labels = labels.to(lm_logits.device)
@@ -504,6 +508,15 @@ class CLIPT5ForConditionalGeneration(CLIPT5PreTrainedModel):
         if cont_loss is not None and lm_loss is not None:
             loss = (cont_loss + lm_loss) / 2.0
 
+        if return_dict:
+            decoder_hidden_states = decoder_outputs.hidden_states if decoder_outputs is not None else None
+            decoder_attentions = decoder_outputs.attentions if decoder_outputs is not None else None
+            cross_attentions = decoder_outputs.cross_attentions if decoder_outputs is not None else None
+        else:
+            decoder_hidden_states = decoder_outputs[1] if decoder_outputs is not None else None
+            decoder_attentions = decoder_outputs[2] if decoder_outputs is not None else None
+            cross_attentions = decoder_outputs[3] if decoder_outputs is not None else None
+
         if not return_dict:
             output = ((lm_logits, logits_per_image, logits_per_text, text_embeds, image_embeds,)
                       + decoder_outputs[1:] + encoder_outputs)
@@ -519,9 +532,9 @@ class CLIPT5ForConditionalGeneration(CLIPT5PreTrainedModel):
             text_embeds=text_embeds,
             image_embeds=image_embeds,
             vision_model_output=vision_outputs,
-            decoder_hidden_states=decoder_outputs.hidden_states,
-            decoder_attentions=decoder_outputs.attentions,
-            cross_attentions=decoder_outputs.cross_attentions,
+            decoder_hidden_states=decoder_hidden_states,
+            decoder_attentions=decoder_attentions,
+            cross_attentions=cross_attentions,
             encoder_last_hidden_state=encoder_outputs.last_hidden_state,
             encoder_hidden_states=encoder_outputs.hidden_states,
             encoder_attentions=encoder_outputs.attentions,
